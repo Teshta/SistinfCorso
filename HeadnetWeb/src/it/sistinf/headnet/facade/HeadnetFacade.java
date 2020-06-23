ec51162674ee41b01c02cd38004b4fc56c8c9874
Rieti <Rieti@192.168.1.71>
2020-06-23 12:17:48 +0200

    Modifiche facade

--- HeadnetWeb/src/it/sistinf/headnet/facade/HeadnetFacade.java
+++ HeadnetWeb/src/it/sistinf/headnet/facade/HeadnetFacade.java
@@ -7,7 +7,7 @@ import it.sistinf.headnet.vo.UserVO;
 public interface HeadnetFacade {
 
 	UserVO cercaUsername(String username) throws Exception;
-
 	void registraUser(UserVO user) throws SQLException;
+	UserVO cercaUtente(String nome, String cognome);
 
 }
